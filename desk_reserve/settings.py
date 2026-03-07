9c0dafb0d72cc6c2ee39b39d206fe5a12e83674d
busuttilc <callum.busuttil@booking.com>
2026-03-07 11:23:34 +0000

    chore: configure postgres database connection

--- desk_reserve/settings.py
+++ desk_reserve/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from pathlib import Path
@@ -73,10 +74,10 @@ WSGI_APPLICATION = 'desk_reserve.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
